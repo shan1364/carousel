63b9d61b5b84f326d807fce639a1c7ba2e8167a3
shan1364 <yanshan1364@gmail.com>
2019-10-15 14:49:10 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,76 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+
+<div class="component demo">
+
+<div class="carousel slide demo" id="carousel-demo">
+    <ol class="carousel-indicators">
+      <li data-target="#carousel-demo" data-slide-to="0" class="active"></li>
+      <li data-target="#carousel-demo" data-slide-to="1"></li>
+      <li data-target="#carousel-demo" data-slide-to="2"></li>
+    </ol>
+
+      <div class="carousel-inner"> <!--幻燈片項目-->
+      <div class="carousel-item active">
+        <img class="d-block w-100" src="./assets/1.jpg" alt="" />
+      </div>
+      <div class="carousel-item">
+        <img class="d-block w-100" src="./assets/2.jpg" alt="" />
+      </div>
+      <div class="carousel-item">
+        <img class="d-block w-100" src="./assets/3.jpg" alt="" />
+      </div>
+    </div>
+
+     <a href="#carousel-demo" class="carousel-control-prev" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+    </a>
+    <a href="#carousel-demo" class="carousel-control-next" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+    </a>
   </div>
+
+
+</div>
+
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
 import HelloWorld from './components/HelloWorld.vue'
-
-@Component({
+import $ from 'jquery'
+export default {data() {
+    return {}
+  },
   components: {
     HelloWorld
-  }
+  },
+  created() {
+    $(document).ready(() => {
+  $('.carousel').carousel({
+    interval: 100
+  })
 })
-export default class App extends Vue {}
+
+  }
+}
+
+
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+.component.demo {
+  padding: 50px;
 }
+
+.carousel.demo .carousel-item {
+  height: 400px;
+}
+
+.carousel.demo .carousel-item img {
+  min-height: 400px;
+  max-height: 600px;
+  object-fit: cover; 
+}
+
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import Vue from 'vue'
+import 'bootstrap'; // Import js file
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import css file
 import App from './App.vue'
 
 Vue.config.productionTip = false
